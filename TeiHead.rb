2a768eda273a749ddc1ebb77adc4b3f7a2cec674
jrgriffiniii <griffinj@lafayette.edu>
2014-11-02 11:50:38 -0500

    SPP-113 #time 5m Extending the handling for decorative literals for the case identified in 193-05P1 (headnote 3)

--- TeiHead.rb
+++ TeiHead.rb
@@ -168,7 +168,7 @@ module SwiftPoemsProject
     
     def push(token)
 
-      # puts 'Adding token ' + token
+      puts 'Adding token ' + token
 
       # if NB_MARKUP_TEI_MAP.has_key? token or (NB_MARKUP_TEI_MAP.has_key? @current_leaf.name and NB_MARKUP_TEI_MAP[@current_leaf.name].has_key? token)
       if NB_SINGLE_TOKEN_TEI_MAP.has_key? token or (NB_TERNARY_TOKEN_TEI_MAP.has_key? @current_leaf.name and NB_TERNARY_TOKEN_TEI_MAP[@current_leaf.name][:secondary].has_key? token) or (NB_MARKUP_TEI_MAP.has_key? @current_leaf.name and NB_MARKUP_TEI_MAP[@current_leaf.name].has_key? token) or NB_MARKUP_TEI_MAP.has_key? token

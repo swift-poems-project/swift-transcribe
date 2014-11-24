e4c9e7ba1f0ae63cb087bcdcc589c357d23bb37b
jrgriffiniii <griffinj@lafayette.edu>
2014-11-24 10:18:13 -0500

    SPP-93 #time 5m Extending FN modecode parsing within titles for "\" characters

--- NotaBeneTitleParser.rb
+++ NotaBeneTitleParser.rb
@@ -96,7 +96,7 @@ module SwiftPoemsProject
 
       @text = @text.gsub(/«FN1/, '«FN1·')
       @text = @text.gsub(/([a-z\.][\d\s]*)»/, '\\1.»')
-      @text = @text.gsub(/([,])»/, '\\1.»')
+      @text = @text.gsub(/([\\,])»/, '\\1.»')
 
       header = TeiHeader.new(@teiParser.headerElement)
 

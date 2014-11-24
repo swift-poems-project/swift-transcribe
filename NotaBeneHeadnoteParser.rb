62d7a99036e4d0ae351029e725cca4c9e9577ac0
jrgriffiniii <griffinj@lafayette.edu>
2014-11-24 10:09:40 -0500

    SPP-93 #time 6m Resolving issues related to FN modecodes which terminate with lower case characters within headnote fields

--- NotaBeneHeadnoteParser.rb
+++ NotaBeneHeadnoteParser.rb
@@ -23,6 +23,7 @@ module SwiftPoemsProject
       # Resolves issues related to certain footnote terminating modecodes
       # See SPP-93
       line = line.gsub /([a-z\.]\d+?)»/, '\\1.»'
+      line = line.gsub /([a-z])»/, '\\1.»'
 
       # For cleaning extraneous MDNM mode codes
       # @todo Refactor into a CSV file for parsing (original line, cleaned line)
